27f271455cdef5e90135990070c9e8460b0fe5b3
Глеб Безруков <bezrukov96@outlook.com>
2020-04-10 17:50:41 +0300

    [3] Базовая реализация сервиса парсинга

---
+++ ConfigEncoder.Core/ParserService/BaseParserService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+
+namespace ConfigEncoder.Core.Services
+{
+    /// <summary>
+    /// Базовая реализация парсера
+    /// </summary>
+    public class BaseParserService : IParserService
+    {
+        public IEnumerable Parser(object config)
+        {
+            object[] sectionKeys = null;
+            if (config == null) throw new ArgumentNullException(nameof(config));
+            if (config is Configuration reducedConfig)
+            {
+                ConfigurationSectionCollection sections = reducedConfig.Sections;
+                sectionKeys = new object[sections.Count];
+                if (sections.Count <= 0) return sectionKeys;
+                for (int i = 0; i < sections.Count; i++)
+                {
+                    var sectionKey = sections.GetKey(i);
+                    sectionKeys[i] = sectionKey;
+                }
+            }
+            return sectionKeys;
+        }
+    }
+}
