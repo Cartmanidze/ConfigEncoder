1262a9b2eb72a620f26b9dd4325f37f757a5dbbb
Глеб Безруков <bezrukov96@outlook.com>
2020-04-11 09:41:21 +0300

    Базовая реализация шифрования

--- ConfigEncoder.Core/EncryptionService/BaseEncryptionService.cs
+++ ConfigEncoder.Core/EncryptionService/BaseEncryptionService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using ConfigEncoder.Core.Interfaces;
 using Microsoft.Extensions.Logging;
 
@@ -6,13 +7,36 @@ namespace ConfigEncoder.Core.Services
 {
     public class BaseEncryptionService : BaseService, IEncryptionService
     {
+        private const string _provider = "RsaProtectedConfigurationProvider";
+
         public BaseEncryptionService(ILogger<BaseService> logger) : base(logger)
         {
         }
 
         public bool Encryption(object config, string key)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return EncryptionProcessing(config, key);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, $"Ошибка : {ex.Message}");
+                throw;
+            }
+        }
+
+        private bool EncryptionProcessing(object config, string key)
+        {
+            if (config == null) throw new ArgumentNullException(nameof(config));
+            if (!(config is Configuration reducedConfig)) return false;
+            ConfigurationSection section = reducedConfig.GetSection(key);
+            if (section == null) return false;
+            Logger.LogInformation($"{section} получено");
+            section.SectionInformation.ProtectSection(_provider);
+            section.SectionInformation.ForceSave = true;
+            reducedConfig.Save(ConfigurationSaveMode.Full);
+            return true;
         }
     }
 }
