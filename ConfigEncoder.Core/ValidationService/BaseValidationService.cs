749bf8e6ea7d859bbae5b660cbe85f26689e69b7
Глеб Безруков <bezrukov96@outlook.com>
2020-04-11 11:13:40 +0300

    Доработка сервиса валидации

--- ConfigEncoder.Core/ValidationService/BaseValidationService.cs
+++ ConfigEncoder.Core/ValidationService/BaseValidationService.cs
@@ -13,25 +13,45 @@ namespace ConfigEncoder.Core.ValidationService
 
         public bool IsValidConfiguration(object config, string key)
         {
-            if (config == null) throw new ArgumentNullException(nameof(config));
-            if (!(config is Configuration reducedConfig)) return false;
-            return IsValidSectionKey(key) && (!IsProtected(reducedConfig, key) && !IsLocked(reducedConfig, key));
+            try
+            {
+                if (config == null) throw new ArgumentNullException(nameof(config));
+                if (!(config is Configuration reducedConfig)) return false;
+                return IsValidSectionKey(key) && (!IsProtected(reducedConfig, key) && !IsLocked(reducedConfig, key));
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, $"Ошибка валидации : {ex.Message}");
+                throw;
+            }
         }
 
         private bool IsLocked(Configuration config, string sectionKey)
         {
             ConfigurationSection section = config.GetSection(sectionKey);
+            if (section.SectionInformation.IsLocked)
+            {
+                Logger.LogWarning($"Секция {section.SectionInformation.SectionName} заблокирована от шифрования");
+            }
             return section.SectionInformation.IsLocked;
         }
 
         private bool IsValidSectionKey(string sectionKey)
         {
+            if (!string.IsNullOrEmpty(sectionKey) && !string.IsNullOrWhiteSpace(sectionKey))
+            {
+                Logger.LogWarning($"Ключ {sectionKey} не валиден");
+            }
             return !string.IsNullOrEmpty(sectionKey) && !string.IsNullOrWhiteSpace(sectionKey);
         }
 
         private bool IsProtected(Configuration config, string sectionKey)
         {
             ConfigurationSection section = config.GetSection(sectionKey);
+            if (section.SectionInformation.IsProtected)
+            {
+                Logger.LogWarning($"Секция {section.SectionInformation.SectionName} защищена от шифрования");
+            }
             return section.SectionInformation.IsProtected;
         }
     }
