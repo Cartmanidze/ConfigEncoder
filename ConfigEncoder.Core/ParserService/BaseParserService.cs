7913887b03cbac78ad54bae63c54dfd5a993bf85
Глеб Безруков <bezrukov96@outlook.com>
2020-04-11 09:01:54 +0300

    Изменене реализации парсера

--- ConfigEncoder.Core/ParserService/BaseParserService.cs
+++ ConfigEncoder.Core/ParserService/BaseParserService.cs
@@ -1,28 +1,41 @@
 ﻿using System;
 using System.Collections;
 using System.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace ConfigEncoder.Core.Services
 {
     /// <summary>
     /// Базовая реализация парсера
     /// </summary>
-    public abstract class BaseParserService : IParserService
+    public class BaseParserService : BaseService, IParserService
     {
-        public virtual IEnumerable Parser(object config)
+        public IEnumerable Parser(object config)
+        {
+            try
+            {
+                return ParserProcessing(config);
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e, e.Message);
+                throw;
+            }
+        }
+
+        private IEnumerable ParserProcessing(object config)
         {
             object[] sectionKeys = null;
             if (config == null) throw new ArgumentNullException(nameof(config));
-            if (config is Configuration reducedConfig)
+            if (!(config is Configuration reducedConfig)) return null;
+            ConfigurationSectionCollection sections = reducedConfig.Sections;
+            sectionKeys = new object[sections.Count];
+            if (sections.Count <= 0) return null;
+            for (var i = 0; i < sections.Count; i++)
             {
-                ConfigurationSectionCollection sections = reducedConfig.Sections;
-                sectionKeys = new object[sections.Count];
-                if (sections.Count <= 0) return sectionKeys;
-                for (int i = 0; i < sections.Count; i++)
-                {
-                    var sectionKey = sections.GetKey(i);
-                    sectionKeys[i] = sectionKey;
-                }
+                var sectionKey = sections.GetKey(i);
+                sectionKeys[i] = sectionKey;
+                Logger.LogInformation($"{sectionKey} был добавлен");
             }
             return sectionKeys;
         }
