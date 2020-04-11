869867893f2cc7956deadbb52fa419f848116080
Глеб Безруков <bezrukov96@outlook.com>
2020-04-11 09:01:28 +0300

    Добавление базового сервиса

---
+++ ConfigEncoder.Core/BaseService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ConfigEncoder.Core
+{
+    public abstract class BaseService
+    {
+        protected ILogger<BaseService> Logger { get; set; }
+    }
+}
