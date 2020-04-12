94b3b3572ad383e8178b5358a4b22ed450dc50de
Глеб Безруков <Gleb.Bezrukov@galaktikapro.onmicrosoft.com>
2020-04-12 15:22:25 +0300

    Добавление интерфейса для инициализации объекта конфигурации

---
+++ ConfigEncoder.Core/Interfaces/IConfigurationInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConfigEncoder.Core.Interfaces
+{
+    /// <summary>
+    /// Инициализация объекта конфигурации
+    /// </summary>
+    public interface IConfigurationInitializer
+    {
+        /// <summary>
+        /// Инициализация объекта конфигурации
+        /// </summary>
+        /// <param name="path">Путь до файла конфигурации</param>
+        /// <returns>Объект конфигурации</returns>
+        object Initialize(string path);
+    }
+}
