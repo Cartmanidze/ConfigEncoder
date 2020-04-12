10c7fe090cfa8affa2ad79922c9d8c8647835f68
Глеб Безруков <Gleb.Bezrukov@galaktikapro.onmicrosoft.com>
2020-04-12 15:47:38 +0300

    Исправление возвращаемого объекта в методе Initialize

--- ConfigEncoder.Core/Interfaces/IConfigurationInitializer.cs
+++ ConfigEncoder.Core/Interfaces/IConfigurationInitializer.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ConfigEncoder.Data;
 
 namespace ConfigEncoder.Core.Interfaces
 {
     /// <summary>
     /// Инициализация объекта конфигурации
     /// </summary>
-    public interface IConfigurationInitializer
+    public interface IConfigurationInitializer<TConfiguration> where TConfiguration : class
     {
         /// <summary>
         /// Инициализация объекта конфигурации
         /// </summary>
         /// <param name="path">Путь до файла конфигурации</param>
         /// <returns>Объект конфигурации</returns>
-        object Initialize(string path);
+        ICustomerConfiguration<TConfiguration> Initialize(string path);
     }
 }
